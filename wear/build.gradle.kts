6b87958c5aad943c06fbcaa9639ce1dc22985582
Marcus Hitchins <marcus@hhost.me>
2026-09-22 18:48:12 +0100

    add wear os support

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -1,46 +1,6 @@
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.application)
-    alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.kotlin.compose)
-}
-
-kotlin {
-    androidTarget()
-    
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(project(":core:domain"))
-                implementation(project(":core:data"))
-                implementation(project(":core:ui"))
-
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.ui)
-
-                implementation(libs.androidx.wear.compose.material3)
-                implementation(libs.androidx.wear.compose.foundation)
-                implementation(libs.androidx.wear.compose.navigation)
-
-                implementation(libs.kotlinx.coroutines.core)
-                implementation(libs.kotlinx.datetime)
-            }
-        }
-        
-        val androidMain by getting {
-            dependencies {
-                implementation(libs.androidx.activity.compose)
-                implementation(libs.androidx.core.ktx)
-                implementation(libs.play.services.location)
-                implementation(libs.kotlinx.coroutines.play.services)
-                implementation(libs.kotlinx.coroutines.android)
-                implementation(libs.androidx.lifecycle.runtime.ktx)
-                implementation(libs.androidx.lifecycle.viewmodel.compose)
-                implementation(libs.androidx.lifecycle.runtime.compose)
-            }
-        }
-    }
 }
 
 android {
@@ -67,11 +27,27 @@ android {
     buildFeatures {
         compose = true
     }
+}
 
-    sourceSets {
-        getByName("main") {
-            manifest.srcFile("src/main/AndroidManifest.xml")
-            res.srcDirs("src/main/res")
-        }
-    }
+dependencies {
+    implementation(project(":core:domain"))
+    implementation(project(":core:data"))
+    implementation(project(":core:ui"))
+
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.wear.compose.material3)
+    implementation(libs.androidx.wear.compose.foundation)
+    implementation(libs.androidx.wear.compose.navigation)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.datetime)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.play.services.location)
+    implementation(libs.kotlinx.coroutines.play.services)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 }
